0deace64fbc4d45a9f8bc3a6df058eab882586d3
rohran01 <anthonytrohr@gmail.com>
2015-12-16 14:34:35 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var counter = 0;
+
+$("document").ready(function() {
+  $(".generate").on("click", appendDom);
+  $(".body").on("click", ".delete", remove);
+  $(".body").on("click", ".change", change);
+});
+
+
+
+function appendDom() {
+  counter++;
+  $(".body").append("<div class='container'></div>");
+
+  var $el = $(".body").children().last();
+  $el.append("<p>" + counter + "</p>");
+  $el.append("<button class='delete'>DELETE</button>");
+  $el.append("<button class='change'>CHANGE</button>");
+};
+function remove() {
+  $(this).parent().remove();
+};
+function change() {
+  $(this).parent().toggleClass("red");
+};
