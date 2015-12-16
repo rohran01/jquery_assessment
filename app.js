ce6f802420c1a42caf1d334d298f36c3d4151f6c
rohran01 <anthonytrohr@gmail.com>
2015-12-16 14:37:55 -0600

    updated jquery

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var counter = 0;
-
-$("document").ready(function() {
+///DOC READY///
+$("document").ready(init);
+///FUNCTIONS///
+function init() {
+  enable()
+};
+function enable() {
   $(".generate").on("click", appendDom);
   $(".body").on("click", ".delete", remove);
   $(".body").on("click", ".change", change);
-});
-
-
-
+}
 function appendDom() {
   counter++;
   $(".body").append("<div class='container'></div>");
-
   var $el = $(".body").children().last();
   $el.append("<p>" + counter + "</p>");
   $el.append("<button class='delete'>DELETE</button>");
